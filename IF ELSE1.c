b6bedd12dd3d349f11436e4171d27c8b4a26bf45
PRAGATIRAWAT09 <pragatirawat2309@gmail.com>
2025-09-05 17:07:46 +0530

    Update IF ELSE1.c

--- IF ELSE1.c
+++ IF ELSE1.c
@@ -4,9 +4,9 @@ void main()
  int num;
  printf("ENTER A NUMBER:");
  scanf("%d",&num);
- if num>0
+ if (num>0)
    printf("%d is positive",num);
- elseif num<0
+ else if (num<0)
    printf("%d is negative",num);
  else
    printf("It is zero.");
