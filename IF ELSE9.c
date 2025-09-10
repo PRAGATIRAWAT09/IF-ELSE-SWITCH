0196bed3b481395d8f8b384852d9a179328cf4e9
PRAGATIRAWAT09 <pragatirawat2309@gmail.com>
2025-09-10 20:49:56 +0530

    Update IF ELSE9.c

--- IF ELSE9.c
+++ IF ELSE9.c
@@ -3,11 +3,12 @@ void main()
 {
 int a;
 clrscr();
-printf("ENTER A NUMBER:");
+printf("ENTER A NUMBER:");//check whether a number is divisible by both 5 and 11.
 scanf("%d",&a);
 if((a%5==0)&&(a%11==0))
   printf("The number %d is divisible by both 5 and 11",a);
 else
   printf("The number %d is not divisible by both 5 and 11",a);
 getch();
+
 }
