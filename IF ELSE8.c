f5b23109df0cc9fda4b79ca0ee8c400758272345
PRAGATIRAWAT09 <pragatirawat2309@gmail.com>
2025-09-10 20:48:54 +0530

    Update IF ELSE8.c

--- IF ELSE8.c
+++ IF ELSE8.c
@@ -3,11 +3,12 @@ void main()
 {
 int num;
 clrscr();
-printf("ENTER A NUMBER:");
+printf("ENTER A NUMBER:");//finding the absolute value of a number.
 scanf("%d",&num);
 if(num<0)
   printf("The absolute value of %d is %d",a,-a);
 else
   printf("The absolute value of %d is %d",a,a);
 getch();
+
 }
