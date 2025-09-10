a8a36db4e31137ad024ca57b357a5ef2a887d6cc
PRAGATIRAWAT09 <pragatirawat2309@gmail.com>
2025-09-10 20:45:23 +0530

    Update IF ELSE2.c

--- IF ELSE2.c
+++ IF ELSE2.c
@@ -3,11 +3,12 @@ void main()
 {
 int num;
 clrscr();
-printf("ENTER A NUMBER:");
+printf("ENTER A NUMBER:");//check whether a number is odd or even.
 scanf("%d",&num);
 if(num%2==0)
   printf("The number %d is even",num);
 else 
   printf("The number %d is odd",num);
 getch();
+
 }
